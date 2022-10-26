fc97364430b326db6f025230c7081a7c55451ceb
Akash JS <akashjs710@gmail.com>
2022-10-26 14:21:02 +0530

    tested calculate func

--- mian_test.go
+++ mian_test.go
@@ -3,12 +3,13 @@ package main
 import "testing"
 
 func TestGreet(t *testing.T) {
-	//initial := Greet()
 	if Greet() != "Welcome to testing and integration" {
 		t.Errorf("\nWanted - 'Welcome to testing and integration'\n got -")
 	}
 }
 
 func TestCalculate(t *testing.T) {
-
+	if Calculate(2) != 4 {
+		t.Error("/n/n Expected 4 ")
+	}
 }
